9db9ae42643c9e836d7fabdfbf2871ca12602e6b
Cristian P <whois.cris@gmail.com>
2022-12-18 22:31:56 -0800

    crawler init

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -20,13 +20,17 @@ class Result:
 class Crawler:
 
 
-    def __init__(self):
-        pass
+    def __init__(self, seed_url: str, download_dir: str):
+
+        if not os.path.exists(download_dir):
+            os.makedirs(download_dir, exist_ok=True)
+        self._download_dir = download_dir
+        self._seed_url = seed_url
 
     def extract_urls(self, url: str):
         pass
 
-    def _download(self, url: str):
+    def _download(self, url: str, out
         pass
 
     def download_resources(self, List[str]):
